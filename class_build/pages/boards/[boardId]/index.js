6c32b1bdb0d6a6ac92fd3c5222764455e0c67bfa
jaymin-developer <kjmkjm822@naver.com>
2022-03-09 15:04:31 +0900

    og업데이트

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function BoardsDetailPage() {
@@ -5,8 +6,22 @@ export default function BoardsDetailPage() {
 
   return (
     <div>
-      안녕하세요! 게시글 상세페이지 입니다!! 게시글 ID는 {router.query.boardId}
-      입니다.
+      <Head>
+        <mega property="og:title" content="나의 게시판입니다" />
+        <mega
+          property="og:description"
+          content="저의 게시판에 오신 것을 환영합니다"
+        />
+        <meta
+          property="og:image"
+          content="https://w.namu.la/s/9071d0575b6d14c0d6fc5832e26fe8ef0a298a1abb1d442cc3c865534ec5e949e8a2d195fe425ebb15f2f1f5b270e6b86979bd1e3fcb4e9d9432bdfbf4fb02a6ef1dadc3477ddb5e704cd37314ac39a1"
+        />
+      </Head>
+      <div>
+        안녕하세요! 게시글 상세페이지 입니다!! 게시글 ID는{" "}
+        {router.query.boardId}
+        입니다.
+      </div>
     </div>
   );
 }
