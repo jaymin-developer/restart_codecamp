d5cee9af3c501cccde0e3d821cd15fb38dc12cb9
jaymin-developer <kjmkjm822@naver.com>
2022-03-09 21:33:44 +0900

    update

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp_deploy_05",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: ".boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
